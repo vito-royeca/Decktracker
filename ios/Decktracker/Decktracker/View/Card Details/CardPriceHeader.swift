4cf9192adb5d082520ca8a094fce95c8e1f32052
J Royeca <buttmanizer@gmail.com>
2015-07-28 22:35:45 +0800

    Pod updates.

---
+++ ios/Decktracker/Decktracker/View/Card Details/CardPriceHeader.swift
@@ -0,0 +1,13 @@
+//
+//  CardPriceHeader.swift
+//  Decktracker
+//
+//  Created by Jovit Royeca on 7/25/15.
+//  Copyright (c) 2015 Jovito Royeca. All rights reserved.
+//
+
+import Cocoa
+
+class CardPriceHeader: UIView {
+
+}
