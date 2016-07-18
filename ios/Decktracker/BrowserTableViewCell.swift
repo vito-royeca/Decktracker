6375e38817293be220bc29a8cabd8acbe32447a3
jovito <jovit.royeca@gmail.com>
2016-07-18 11:46:38 +0800

    feat: MTGJSON v3.3.17

---
+++ ios/Decktracker/BrowserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BrowserTableViewCell.swift
+//  Decktracker
+//
+//  Created by Jovit Royeca on 17/07/2016.
+//  Copyright © 2016 Jovit Royeca. All rights reserved.
+//
+
+import UIKit
+
+class BrowserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
